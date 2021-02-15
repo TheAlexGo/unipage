8e8651394306b8db3845623c61b9e8de1ee0c61b
Alexander <alex280702@mail.ru>
2021-02-16 01:12:38 +0300

    Разработка функции для получения текста из API (англ, рус версии) и получение базового текста при ошибке извлечения данных

--- src/store/actions/wordActions.js
+++ src/store/actions/wordActions.js
@@ -1,8 +1,31 @@
 import {KEY_CORRECT, KEY_ERROR, SET_ACCURACY, SET_DATA_TIMER, SET_SPM, SET_TEXT, SET_TIMER_ID} from "../constActions";
+import axios from "axios";
 
 
 export const getTextAction = () => {
+  return async (dispatch) => {
+    let test = new Promise((resolve) => {
+      if(0) {
+        resolve(axios.get('https://baconipsum.com/api/?type=all-meat&sentences=1'))
+      } else {
+        resolve(axios.get('https://fish-text.ru/get?type=sentence&number=1'))
+      }
+    })
+    const response = await(test.then((response) => response).catch(() =>
+      ({data: ['Жили у бабуси два весёлых гуся: один серый, другой белый, два весёлых гуся!']})
+    ))
 
+    let responseText
+    if(0) {
+      responseText = response.data[0]
+    } else {
+      responseText = response.data.text
+    }
+
+    let text = [...responseText].map((s, index) => ({symb: s, class: 'normal-b', id: index}));
+    text[0] = {...text[0], class: 'active-b'};
+    dispatch(setText(text));
+  }
 }
 
 export const keyCorrect = (str) =>
