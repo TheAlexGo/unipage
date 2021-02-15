1e532ba21c716e8d36f6ff9f152632f961f5be79
Alexander <alex280702@mail.ru>
2021-02-16 01:50:09 +0300

    Создание функции обработки ивента нажатия на клавишу. Установка различных классов состояний буквы

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,54 @@
 import React from "react";
 import {Words} from "./Words";
 import {useDispatch, useSelector} from "react-redux";
+import {useCallback, useEffect} from "react/cjs/react.production.min";
+import {store} from "../store/store";
+import {keyCorrect, keyError} from "../store/actions/wordActions";
 
 function Main() {
   const dispatch = useDispatch();
   const text = useSelector(state => state.words.str);
+  const indexSymb = useSelector(state => state.words.indexSymb);
+
+  const onKeyPressed = useCallback((e) => {
+      const currentObj = text[indexSymb];
+      const nextObj = text[indexSymb + 1];
+
+      if (e.location !== 0 || indexSymb === text.length) return;
+
+      if (e.key === currentObj.symb) {
+        text[indexSymb] = {...currentObj, class: 'passed-b'};
+        if (text[indexSymb + 1])
+          text[indexSymb + 1] = {...nextObj, class: 'active-b'};
+
+        dispatch(keyCorrect(text));
+      } else {
+        if(currentObj.class === 'active-b')
+        {
+          text[indexSymb] = {...currentObj, class: 'error-b'};
+          dispatch(keyError(text))
+        }
+      }
+
+    }, [dispatch, indexSymb, text]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyPressed);
+    if(indexSymb === text.length && text.length) {
+      const error = store.getState().words.error;
+      const current = store.getState().words.current;
+
+      console.log(`Верно: ${current}\nНеверно: ${error}`);
+    }
+    return () => {
+      document.removeEventListener("keydown", onKeyPressed);
+    }
+  }, [indexSymb, onKeyPressed, text]);
 
   return (
-    <div className='text-block'>
+    <div className='text-block'
+         onKeyDown={onKeyPressed}>
       <Words words={ text }/>
     </div>
   )
